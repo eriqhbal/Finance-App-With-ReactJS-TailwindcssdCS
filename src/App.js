47f12fb560a7cd8ca29ff8fb213b8f28f13c2ec4
Zuleriqhbal <eriqhbal@gmail.com>
2022-11-15 17:55:49 +0700

    Menambahkan component newslatter ke app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react';
 // Import Components
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Analytics from './components/Analytics';
+import Newslatter from './components/Newslatter';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Analytics/>
+      <Newslatter/>
     </div>
   );
 }
